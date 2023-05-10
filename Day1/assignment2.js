5e47b97545d5f68b03bf61a406e8d478140c8ed1
Layahmed <121787660+Layahmed@users.noreply.github.com>
2023-05-10 17:12:45 +0530

    Add files via upload

---
+++ Day1/assignment2.js
@@ -0,0 +1,9 @@
+function isInteger(number){
+    return Number.isInteger(number);
+
+}
+var num1 = 20;
+var num2 = 1.2;
+
+console.log(isInteger(num1));
+console.log(isInteger(num2));
---
+++ Day1/assignment3.js
@@ -0,0 +1,6 @@
+var today = new Date();
+var year = today.getFullYear();
+var month = today.getMonth() + 1;
+var day = today.getDate();
+
+console.log("Today is: " + year +"-"+ month + "-" +day);
